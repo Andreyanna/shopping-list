efa72eb8aa4e02fcbe98310cbc9c3b8b20e97fa0
Andreyanna <andreyanna.it@gmail.com>
2023-02-06 00:11:20 +0800

    Update script.js
    Выполнила все задания и оставила комментарии для наставника (некорректно работает зачеркивание пунктов).

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-/* Новые элементы должны добавляться в список по нажатию на Enter */
+const inputLine = document.querySelector('#input');
+const itemContainer = document.querySelector('#items')
 
-/* Пустые элементы не должны добавляться */
+inputLine.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const newItemText = inputLine.value;
+        const newItem = document.createElement('li');
+        /*newItem.classList.add('new-items'); это лишнее наверно */
+        newItem.textContent = newItemText;
+        const eachNewItem = document.querySelectorAll('li');
+        for (let task of eachNewItem) {
+            task.addEventListener('click', function() {
+                task.classList.toggle('done');
+            })
+        };
+        if (newItemText != '') {
+            itemContainer.append(newItem);
+        }
+        inputLine.value = '';
+        /* пробовала вставить эту конструкцию if в разные места - работает только тут, но по-прежнему коряво - через пункт. Закономерность отследить не удалось и найти ошибку тоже пока для меня трудно. Надеюсь получить обьяснение от специалиста ) */
+    }
+});
 
-/* Если кликнуть на элемент списка, он зачеркивается */
 
-/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
-
-/* Очищать input после добавления нового элемента в список */
+/* const eachNewItem = document.querySelectorAll('.new-items');
+for (let task of eachNewItem) {
+    task.addEventListener('click', function() {
+        task.classList.toggle('done');
+    })
+}; /* а почему не может работать за пределами функции event? Ведь она отвечает за то, чтобы появлялись li. А клики на них - отдельные уже действия, идут следом. Я так изначально рассуждала и поэтому присвоила им класс. Хочется узнать, почему такая логика неверна, почему нужно все внутрь именно той функции вставлять */
