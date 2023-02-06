59e7362dd4a5e914335fbd2a4aa8b7871d70c6c3
Andreyanna <andreyanna.it@gmail.com>
2023-02-06 21:03:39 +0800

    Исправила ошибку с зачеркиванием пунктов

--- script.js
+++ script.js
@@ -5,26 +5,13 @@ inputLine.addEventListener('keydown', function(event) {
     if (event.key == 'Enter') {
         const newItemText = inputLine.value;
         const newItem = document.createElement('li');
-        /*newItem.classList.add('new-items'); это лишнее наверно */
         newItem.textContent = newItemText;
-        const eachNewItem = document.querySelectorAll('li');
-        for (let task of eachNewItem) {
-            task.addEventListener('click', function() {
-                task.classList.toggle('done');
-            })
-        };
+        newItem.addEventListener('click', function() {
+            newItem.classList.toggle('done');
+        });
         if (newItemText != '') {
             itemContainer.append(newItem);
         }
         inputLine.value = '';
-        /* пробовала вставить эту конструкцию if в разные места - работает только тут, но по-прежнему коряво - через пункт. Закономерность отследить не удалось и найти ошибку тоже пока для меня трудно. Надеюсь получить обьяснение от специалиста ) */
     }
 });
-
-
-/* const eachNewItem = document.querySelectorAll('.new-items');
-for (let task of eachNewItem) {
-    task.addEventListener('click', function() {
-        task.classList.toggle('done');
-    })
-}; /* а почему не может работать за пределами функции event?  Ведь она отвечает за то, чтобы появлялись li. А клики на них - отдельные уже действия, идут следом. Я так изначально рассуждала и поэтому присвоила им класс. Хочется узнать, почему такая логика неверна, почему нужно все внутрь именно той функции вставлять */
